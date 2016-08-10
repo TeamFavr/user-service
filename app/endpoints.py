db82da9d52244437e293aa11e5067cd6fec4d88c
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2016-08-10 16:28:49 +0100

    Add friendship model and listing of friends

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify, request
+"""Contains the routes for the user Blueprint."""
+from flask import Blueprint, jsonify, request, g
 
 from flask_bcrypt import check_password_hash
 
@@ -7,14 +8,29 @@ from .models import db, User
 
 user = Blueprint('user', __name__)
 
+def before_request():
+    id = request.headers.get("User-Id", None)
+    if id:
+        user = User.query.get(id)
+        if user is None:
+            raise CustomError(400, message="Invalid User-Id in Header.")
+        g.user = user
+    else:
+        g.user = None
+
+user.before_request(before_request)
+
 
 @user.route("/")
 def index():
+    """Index route."""
+    print(request.headers)
     return 'User index'
 
 
 @user.route("/authenticate", methods=["POST"])
 def authenticate():
+    """Return whether or not an email or password combination is valid."""
     # Get JSON data from request
     json = request.get_json()
 
@@ -35,6 +51,7 @@ def authenticate():
 
 @user.route("/signup", methods=["POST"])
 def signup():
+    """Create new User from supplied infomation."""
     # Get JSON from request
     json = request.get_json()
     if json is None:
@@ -46,12 +63,11 @@ def signup():
         raise CustomError(400, message="Must include a first name, last name,"
                                        "email and password.")
 
-
-    # Check if email is unique
+    # # Check if email is unique
     if User.query.filter_by(email=json['email']).first() is not None:
         raise CustomError(409, message='Email already in use.')
 
-    # TODO: Add password validation
+    # # TODO: Add password validation
 
     user = User(json['first_name'], json['last_name'], json['email'],
                 json['password'])
@@ -63,8 +79,34 @@ def signup():
 
 @user.route("/users/<int:id>")
 def get_user_by_id(id):
+    """Get a user by its id."""
     user = User.query.get(id)
     if user is None:
         raise CustomError(404, 'User with id: {} was not found.'.format(id))
 
     return jsonify({'success': True, 'user': user.to_dict()})
+
+
+@user.route("/friends")
+def friends():
+    """Return all of the current user's friends."""
+    friends = [u.to_dict() for u in g.user.get_friends()]
+    return jsonify({'success': True, 'friends': friends})
+
+
+@user.route("/friend-requests")
+def friend_requests():
+    """Return all friendships which are not confirmed for the current user."""
+    pass
+
+
+@user.route("/friend-request", methods=["POST"])
+def create_friend_request():
+    """Create an unconfirmed friendship between two users."""
+    pass
+
+
+@user.route("/friend-request/<int:id>", methods=["GET", "PATCH", "DELETE"])
+def get_friend_request_with_id():
+    """Get, update or delete friendship with the specified id."""
+    pass
--- app/models.py
+++ app/models.py
@@ -1,9 +1,12 @@
+"""Models representing tables in the database."""
 from app import db
 
 from flask_bcrypt import generate_password_hash
 
 
 class User(db.Model):
+    """Represents a user."""
+
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(120))
     last_name = db.Column(db.String(120))
@@ -12,12 +15,14 @@ class User(db.Model):
     is_ad_free = db.Column(db.Boolean(), default=False)
 
     def __init__(self, first_name, last_name, email, password):
+        """Create a User object but not save it to the database."""
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = generate_password_hash(password)
 
     def to_dict(self):
+        """Convert instance into a dict, excluding password."""
         return {
             'id': self.id,
             'first_name': self.first_name,
@@ -25,3 +30,48 @@ class User(db.Model):
             'email': self.email,
             'is_ad_free': self.is_ad_free
         }
+
+    def get_friends(self):
+        """Return User objects for each of the instance's friends."""
+        friendships = Friendship.query.filter(
+            Friendship.actioning_user_id == self.id or
+            Friendship.recieving_user_id == self.id,
+            Friendship.confirmed is True
+        )
+
+        user_ids = set()
+        for friendship in friendships:
+            user_ids.add(friendship.actioning_user_id)
+            user_ids.add(friendship.recieving_user_id)
+
+        user_ids.discard(self.id)
+
+        return User.query.filter(User.id.in_(user_ids)).all()
+
+    def get_friend_requests(self):
+        """Return all Friendship objects which the User has not accepted."""
+        friend_requests = Friendship.query.filter(
+            Friendship.recieving_user_id == self.id,
+            Friendship.confirmed is False
+        )
+
+        return friend_requests
+
+
+class Friendship(db.Model):
+    """Represents a relationship between two Users."""
+
+    __table_args__ = (
+        db.UniqueConstraint('actioning_user_id', 'recieving_user_id'),
+        {}
+    )
+    id = db.Column(db.Integer, primary_key=True)
+    actioning_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recieving_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    confirmed = db.Column(db.Boolean(), default=False)
+
+    def __init__(self, actioning_user, recieving_user, confirmed=False):
+        """Create a Friendship object."""
+        self.actioning_user_id = actioning_user.id
+        self.recieving_user_id = recieving_user.id
+        self.confirmed = confirmed
